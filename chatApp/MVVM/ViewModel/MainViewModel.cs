a1246bad60056976658ee75409e8fceb93916ddd
ajyol <ajyol@LUNI>
2024-02-23 16:01:26 -0600

    Add project files.

---
+++ chatApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using ChatClient.MVVM.Corre;
+using ChatClient.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public RelayCommand ConnectToServerCommand { get; set; }
+
+        public string Username { get; set; }
+
+        private Server _server { get; set; }
+        public MainViewModel() 
+        {
+            _server = new Server();
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer(Username), o => !string.IsNullOrEmpty(Username));
+        }
+    }
+}
